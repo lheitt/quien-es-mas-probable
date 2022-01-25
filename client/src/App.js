0a81f7bdf8ad3e8fa70c34f82260fe0c948a3647
Luciano Heitt <lheitt@gmail.com>
2022-01-24 21:07:31 -0300

    online game added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Game from "./components/Game/Game";
+import OnlineGame from "./components/Game/OnlineGame";
 import NewQuestion from "./components/NewQuestion/NewQuestion";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/game/:room" element={<Game />} />
+                <Route path="/online-game/:room" element={<OnlineGame />} />
                 <Route path="/new-question" element={<NewQuestion />} />
             </Routes>
         </React.Fragment>
