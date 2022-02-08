3b5b5d3d3c3bd9549cf6a848f20042d90cfc941d
Luciano Heitt <lheitt@gmail.com>
2022-02-08 20:34:03 -0300

    local game eliminated and room exist function created

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Game from "./components/Game/Game";
-import OnlineGame from "./components/Game/OnlineGame";
 import NewQuestion from "./components/NewQuestion/NewQuestion";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/game/:room" element={<Game />} />
-                <Route path="/online-game/:room" element={<OnlineGame />} />
                 <Route path="/new-question" element={<NewQuestion />} />
             </Routes>
         </React.Fragment>
