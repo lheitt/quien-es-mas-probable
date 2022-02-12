9ae9304d31850d5cabe3e1bfef2936a0b1cf7c28
Luciano Heitt <lheitt@gmail.com>
2022-02-12 04:28:18 -0300

    new question mobile screen done

--- mobile/components/Home/GameMode.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function GameMode({ navigation }) {
-    return (
-        <View>
-            <Text>GameMode</Text>
-        </View>
-    );
-}
